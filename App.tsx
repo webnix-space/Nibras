0f46e70163bfba783d3ac5410b7ce66000d2b0d5
Webnix <sanibmalik850@gmail.com>
2026-07-28 16:59:44 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import GuardModeScreen from './src/screens/GuardModeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar style="light" />
+      <Stack.Navigator screenOptions={{ headerStyle: { backgroundColor: '#0B0F14' }, headerTintColor: '#F3F4F6' }}>
+        <Stack.Screen name="Guard Mode" component={GuardModeScreen} />
+        {/* TODO: Vault Mode screen, Pre-Deploy Checklist screen */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
